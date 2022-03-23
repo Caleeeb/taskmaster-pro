3da8fd26397834cb14b9cbb540da4a189c8934c3
Caleeeb <daycinematography@gmail.com>
2022-03-23 18:11:40 -0400

    added drag and drop functionality to task items

--- assets/js/script.js
+++ assets/js/script.js
@@ -50,19 +50,21 @@ $(".list-group").on("click", "p", function () {
   var text = $(this)
     .text()
     .trim();
+
+  // replace p element with a new textarea  
   var textInput = $("<textarea>")
     .addClass("form-control")
     .val(text);
   $(this).replaceWith(textInput);
+
+  // auto focus new element
   textInput.trigger("focus");
 });
 
 // value of due date was changed
 $(".list-group").on("blur", "textarea", function () {
   // get the textarea's current value/text
-  var text = $(this)
-    .val()
-    .trim();
+  var text = $(this).val();
 
   // get the parent ul's id attribute
   var status = $(this)
@@ -179,6 +181,75 @@ $("#remove-tasks").on("click", function () {
     $("#list-" + key).empty();
   }
   saveTasks();
+});
+
+$(".card .list-group").sortable({
+  connectWith: $(".card .list-group"),
+  scroll: false,
+  tolerance: "pointer",
+  helper: "clone",
+  activate: function (event) {
+    console.log("activate", this);
+  },
+  deactivate: function (event) {
+    console.log("deactivate", this);
+  },
+  over: function (event) {
+    console.log("over", event.target);
+  },
+  out: function (event) {
+    console.log("out", event.target);
+  },
+  update: function (event) {
+    // array to store the task data in
+    var tempArr = [];
+
+    // loop over current set of children in sortable list
+    $(this).children().each(function () {
+      var text = $(this)
+        .find("p")
+        .text()
+        .trim();
+
+      var date = $(this)
+        .find("span")
+        .text()
+        .trim();
+
+      // add task data to the temp array as an object
+      tempArr.push({
+        text: text,
+        date: date
+      });
+    });
+
+    // trim down list's ID to match object property
+    var arrName = $(this)
+      .attr("id")
+      .replace("list-", "");
+
+    // update array on tasks object and save
+    tasks[arrName] = tempArr;
+    saveTasks();
+
+    console.log(tempArr);
+  }
+});
+
+// makeing the trash/"drop here to remove" bar a droppable space to delete tasks
+$("#trash").droppable({
+  accept: ".card .list-group-item",
+  tolerance: "touch",
+  drop: function (event, ui) {
+    ui.draggable.remove();
+    console.log("drop");
+  },
+  over: function (event, ui) {
+    console.log("over");
+  },
+  over: function (event, ui) {
+    console.log("out");
+  }
 });
 
 // load tasks for the first time
